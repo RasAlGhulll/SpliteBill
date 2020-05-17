599d7b8de0d064c4e18d7efd003c04aa6394971d
shreyans <shreyanssureja@gmail.com>
2020-05-17 08:52:50 +0530

    created CRUD operations for Users

--- src/main/java/com/splitbill/splitbillapp/user/UserController.java
+++ src/main/java/com/splitbill/splitbillapp/user/UserController.java
@@ -1,8 +1,7 @@
 package com.splitbill.splitbillapp.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
@@ -17,5 +16,25 @@ public class UserController {
     @GetMapping(value = "/users")
     public List<User> getAllUsers(){
         return userService.getAllUsers();
+    }
+
+    @GetMapping(value = "/users/{id}")
+    public User getUser(@PathVariable String id){
+        return userService.getUser(id);
+    }
+
+    @PostMapping(value = "/users")
+    public void addUser(@RequestBody User newUser){
+        userService.addUser(newUser);
+    }
+
+    @PutMapping(value = "/users/{id}")
+    public void updateUser(@RequestBody User updatedUser,@PathVariable String id){
+        userService.updateUser(updatedUser,id);
+    }
+
+    @DeleteMapping(value = "/users/{id}")
+    public void deleteUser(@PathVariable String id){
+        userService.deleteUser(id);
     }
 }
