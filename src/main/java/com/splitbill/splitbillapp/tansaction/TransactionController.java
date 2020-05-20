711697761d744897c07600f13e6bacf9ec4b8151
shreyans <shreyanssureja@gmail.com>
2020-05-20 08:53:00 +0530

    updated error handling for database entry

--- src/main/java/com/splitbill/splitbillapp/tansaction/TransactionController.java
+++ src/main/java/com/splitbill/splitbillapp/tansaction/TransactionController.java
@@ -1,13 +1,40 @@
 package com.splitbill.splitbillapp.tansaction;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.splitbill.splitbillapp.customer.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class TransactionController {
 
+    @Autowired
+    private TransactionService transactionService;
+
     @GetMapping(value = "/transcation")
-    public String getTransaction(){
-        return "Hello from transaction";
+    public List<Transaction> getTransaction(){
+        return transactionService.getAllTransaction();
+    }
+
+    @GetMapping(value = "/transcation/{id}")
+    ResponseEntity<Transaction> getTransaction(@PathVariable Long id){
+        return transactionService.getTransaction(id);
+    }
+
+    @PostMapping(value = "/transcation")
+    ResponseEntity<String> addTransaction(@RequestBody Transaction newTransaction){
+        return transactionService.addTransaction(newTransaction);
+    }
+
+    @PutMapping(value = "/transcation/{id}")
+    public void updateTranscation(@RequestBody Transaction updatedTranscation, Long id){
+        transactionService.updateTransaction(updatedTranscation,id);
+    }
+
+    @DeleteMapping(value = "/transcation/{id}")
+    ResponseEntity<String> deleteTranscation(@PathVariable Long id){
+        return transactionService.deleteTranscation(id);
     }
 }
--- src/main/java/com/splitbill/splitbillapp/tansaction/TransactionService.java
+++ src/main/java/com/splitbill/splitbillapp/tansaction/TransactionService.java
@@ -1,4 +1,63 @@
 package com.splitbill.splitbillapp.tansaction;
 
+import com.splitbill.splitbillapp.customer.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class TransactionService {
+
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    public List<Transaction> getAllTransaction(){
+        List<Transaction> transactionList = new ArrayList<>();
+        transactionRepository.findAll().forEach(transactionList::add);
+        return transactionList;
+    }
+
+    ResponseEntity<Transaction> getTransaction(Long id){
+        if(!transactionRepository.existsById(id)){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        else{
+            return new ResponseEntity<>(transactionRepository.findById(id).get(),HttpStatus.OK);
+        }
+    }
+
+    ResponseEntity<String>addTransaction(Transaction newTransaction){
+        if(!customerRepository.existsById(newTransaction.getSenderId()) || !customerRepository.existsById(newTransaction.getReceiverId())){
+            return new ResponseEntity<>("Invalid Request",HttpStatus.BAD_REQUEST);
+        }
+        try{
+            transactionRepository.save(newTransaction);
+        }
+        catch (Exception e){
+            return new ResponseEntity<>("Invalid Request",HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>("Added into the database",HttpStatus.ACCEPTED);
+    }
+
+
+    public void updateTransaction(Transaction updatedTransaction,Long id){
+        transactionRepository.save(updatedTransaction);
+    }
+
+    ResponseEntity<String> deleteTranscation(Long id){
+        try{
+            transactionRepository.deleteById(id);
+        }
+        catch (Exception e){
+            return new ResponseEntity<>("Invalid Request",HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>("Deleted from the database",HttpStatus.OK);
+    }
 }
